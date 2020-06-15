8a0e845a7b96af78a5c4897b0aebbe1e64f848e4
Salman Seifian <salman.seifian@gmail.com>
2020-06-15 18:58:32 +0430

    [+] todos fragment

---
+++ app/src/main/java/com/salmanseifian/shouldoist/todos/TodosFragment.kt
@@ -0,0 +1,10 @@
+package com.salmanseifian.shouldoist.todos
+
+import com.salmanseifian.shouldoist.R
+import com.salmanseifian.shouldoist.base.BaseFragment
+
+class TodosFragment : BaseFragment() {
+    override fun layoutRes(): Int {
+        return R.layout.fragment_todos
+    }
+}
