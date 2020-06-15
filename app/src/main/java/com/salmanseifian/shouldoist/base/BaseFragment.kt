0abf3cdb7ff8f04791817236551f2c1edd9d8cde
Salman Seifian <salman.seifian@gmail.com>
2020-06-15 18:14:33 +0430

    [+] create base fragment

---
+++ app/src/main/java/com/salmanseifian/shouldoist/base/BaseFragment.kt
@@ -0,0 +1,24 @@
+package com.salmanseifian.shouldoist.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment : Fragment() {
+    
+    @LayoutRes
+    internal abstract fun layoutRes(): Int
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(layoutRes(), container, false)
+    }
+
+
+}
