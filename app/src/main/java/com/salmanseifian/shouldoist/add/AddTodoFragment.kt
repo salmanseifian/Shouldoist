066a8485c3c2490a3b6dab7e8b86374a2e03f63d
Salman Seifian <salman.seifian@gmail.com>
2020-06-16 19:23:44 +0430

    [+] add todo fragment

---
+++ app/src/main/java/com/salmanseifian/shouldoist/add/AddTodoFragment.kt
@@ -0,0 +1,11 @@
+package com.salmanseifian.shouldoist.add
+
+import com.salmanseifian.shouldoist.R
+import com.salmanseifian.shouldoist.base.BaseFragment
+
+class AddTodoFragment : BaseFragment() {
+
+    override fun layoutRes(): Int {
+        return R.layout.fragment_add_todo
+    }
+}
--- app/src/main/java/com/salmanseifian/shouldoist/todos/TodosFragment.kt
+++ app/src/main/java/com/salmanseifian/shouldoist/todos/TodosFragment.kt
@@ -1,10 +1,24 @@
 package com.salmanseifian.shouldoist.todos
 
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
 import com.salmanseifian.shouldoist.R
 import com.salmanseifian.shouldoist.base.BaseFragment
+import kotlinx.android.synthetic.main.fragment_todos.*
 
 class TodosFragment : BaseFragment() {
+
     override fun layoutRes(): Int {
         return R.layout.fragment_todos
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fab_add_todo.setOnClickListener {
+            findNavController().navigate(R.id.action_todosFragment_to_addTodoFragment)
+        }
     }
 }
